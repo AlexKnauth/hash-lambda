51e593461221c03f6098623e1fbb4aee9f1542a6
AlexKnauth <alexander@knauth.org>
2014-07-22 10:45:09 -0400

    Update deps

--- info.rkt
+++ info.rkt
@@ -2,5 +2,5 @@
 
 (define collection 'multi)
 
-(define deps '("base" "rackunit-lib" "at-exp-lib" "scribble-lib" "sandbox-lib"))
+(define deps '("base" "rackunit-lib" "at-exp-lib" "scribble-lib" "sandbox-lib" "racket-doc"))
 
