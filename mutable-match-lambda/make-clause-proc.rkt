1d6fdd8438b9244f35c6f9348ab4c87ff8cc24da
AlexKnauth <alexander@knauth.org>
2014-10-27 10:18:00 -0400

    refactor clause->proc

--- mutable-match-lambda/make-clause-proc.rkt
+++ mutable-match-lambda/make-clause-proc.rkt
@@ -2,13 +2,20 @@
 
 (provide make-clause-proc
          clause->proc
+         case-lambda-clause->proc
+         hash-lambda/match-clause->proc
+         match-lambda-clause->proc
+         match-lambda*-clause->proc
          )
 
 (require racket/match
          hash-lambda
          (only-in "communication.rkt" mutable-match-lambda-next)
          (for-syntax racket/base
-                     mutable-match-lambda/syntax-to-string))
+                     syntax/parse
+                     racket/syntax
+                     mutable-match-lambda/syntax-to-string
+                     ))
 
 (define (make-clause-proc test proc)
   (procedure-rename
@@ -25,11 +32,11 @@
     (let* ([str (syntax->string stx)]
            [sym (string->symbol str)])
       (with-syntax ([name (datum->syntax stx `(quote ,sym))])
-        (syntax-case stx ()
-          [(clause->proc #:case-lambda clause)       #'(procedure-rename (case-lambda-clause->proc clause) name)]
-          [(clause->proc #:hash-lambda/match clause) #'(procedure-rename (hash-lambda/match-clause->proc clause) name)]
-          [(clause->proc #:match-lambda clause)      #'(procedure-rename (match-lambda-clause->proc clause) name)]
-          [(caluse->proc #:match-lambda* clause)     #'(procedure-rename (match-lambda*-clause->proc clause) name)]
+        (syntax-parse stx
+          [(clause->proc kw:keyword clause:expr)
+           (define kw-str (keyword->string (syntax-e #'kw)))
+           (with-syntax ([kw-id-clause->proc (format-id #'kw "~a-clause->proc" kw-str #:source #'kw)])
+             #'(procedure-rename (kw-id-clause->proc clause) name))]
           )))))
 
 (define-syntax-rule (case-lambda-clause->proc clause)
