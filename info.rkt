9c606dd15ffc1811254ca47bd0a99b993b91cceb
AlexKnauth <alexander@knauth.org>
2015-04-05 23:41:15 -0400

    fix deps

--- info.rkt
+++ info.rkt
@@ -3,11 +3,13 @@
 (define collection 'multi)
 
 (define deps '("base"
+               "unstable-list-lib"
                "kw-utils"
                "mutable-match-lambda"
                "rackunit-lib"
                "at-exp-lib"
                "scribble-lib"
                "sandbox-lib"
-               "racket-doc"))
+               "racket-doc"
+               ))
 
